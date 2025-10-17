8f8cff5eef5ab192cf0bd92b2b9edc223520bffe
batgaythebatlord <146351648+batgaythebatlord@users.noreply.github.com>
2025-10-17 12:59:01 +0100

    17/10/25

--- comp3016CW1code/comp3016CW1code/comp3016CW1code.cpp
+++ comp3016CW1code/comp3016CW1code/comp3016CW1code.cpp
@@ -1,11 +1,58 @@
-// comp3016CW1code.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿// comp3016CW1code.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//░░░
 
 #include <iostream>
+#include <string>
+#include "mazeHeader.cpp"
+
+using namespace std;
+
+static void drawGrid(bool(&theGrid)[xDist][yDist][4]);
+string draw(bool stuff);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool grid[yDist][xDist][4];
+    bool cellsNotVisited[yDist][xDist];
+
+    drawGrid(grid);
+}
+
+static void drawGrid(bool (&theGrid)[xDist][yDist][4])
+{
+    for (int i = 0; i < sizeof(theGrid) / sizeof(theGrid[0]); i++)
+    {
+        for (int j = 0; j < sizeof(theGrid[0]) / sizeof(theGrid[0][0]); j++)
+        {
+            cout << "||||" + draw(theGrid[i][j][0]) + "||||";
+        }
+
+        cout << "\n";
+
+        for (int j = 0; j < sizeof(theGrid[0]) / sizeof(theGrid[0][0]); j++)
+        {
+            cout << draw(theGrid[i][j][1]) + "    " + draw(theGrid[i][j][3]);
+            // the numbers go ANTICLOCKWISE!!!!!!
+        }
+
+        cout << "\n";
+
+        for (int j = 0; j < sizeof(theGrid[0]) / sizeof(theGrid[0][0]); j++)
+        {
+            cout << "||||" + draw(theGrid[i][j][2]) + "||||";
+        }
+
+        cout << "\n";
+    }
+
+    cout << "\n";
+}
+
+
+string draw(bool stuff)
+{
+    if (!stuff) return "    ";
+    return "||||";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ comp3016CW1code/comp3016CW1code/mazeHeader.cpp
@@ -0,0 +1,2 @@
+constexpr int xDist = 10;
+constexpr int yDist = 10;
